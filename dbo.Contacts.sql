ae0de8149e082c47ac64b1a067b1d005fd19d506
rajawatSwati <swatidrajawat@gmail.com>
2021-06-17 17:37:34 +0530

    Script to create Contacts table

---
+++ dbo.Contacts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Contacts]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [PhoneNumber] NVARCHAR(10) NULL, 
+    [Active] BIT NOT NULL
+)
